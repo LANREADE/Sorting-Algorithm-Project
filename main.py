83e25c51b291cdc28cc74c95c90793f33ca8e81c
lanre <ibrahimlanre1890@gmail.com>
2023-01-13 01:21:01 -0800

    implementing the geometrical visualization of the sorting algorithm

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ class Drawinfo:
      GREY = 128 , 128,128
      BACKGROUND_COLOR = WHITE
 
+     GRADIENTS = [
+          (128 , 128 , 128),
+          (160, 160 , 160),
+          (192 , 192 , 192)
+     ]
+
+     FONT = pygame.font.SysFont('conicsans',30)
+     LARGE_FONT = pygame.font.SysFont('conicsans', 40)
+
      TOP_PAD = 150
      SIDE_PAD = 100
 
@@ -30,6 +39,33 @@ class Drawinfo:
           self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
           self.start_x = self.SIDE_PAD // 2
 
+
+def draw(draw_info):
+     draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+
+     controls = draw_info.FONT.render("R - Reset | SPACE - Start Sorting | A - Ascending | D - Descending", 1, draw_info.BLACK)
+     draw_info.window.blit(controls, (draw_info.width/2 - controls.get_width()/2, 5))
+
+     sorting  = draw_info.FONT.render("I - Insertion sort | B - Bubble sort", 1, draw_info.BLACK)
+     draw_info.window.blit(sorting , (draw_info.width/2 - sorting.get_width()/2, 25))
+     draw_list(draw_info)
+     pygame.display.update()
+
+
+
+
+def draw_list(draw_info):
+     lst = draw_info.lst
+
+     for i , val in enumerate(lst):
+          x = draw_info.start_x + i  * draw_info.block_width
+          y = draw_info.height -  (val - draw_info.min_val) * draw_info.block_height
+
+          color = draw_info.GRADIENTS[i % 3 ]
+          pygame.draw.rect(draw_info.window , color, (x,y ,draw_info.block_width, draw_info.height))
+
+
+
 def generate_starting_list(n , min_val , max_val):
      lst = []
 
@@ -46,16 +82,36 @@ def main():
      min_val = 0
      max_val = 100
      lst = generate_starting_list(n , min_val , max_val)
-     drawinfo = Drawinfo(800, 600 , lst )
+     draw_info = Drawinfo(700,500, lst)
+
+     sorting = False
+     ascending = True
 
      while run:
           clock.tick(60)
 
+          draw(draw_info)
+
           pygame.display.update()
 
           for event in pygame.event.get():
-               if event == pygame.QUIT:
+               if event.type == pygame.QUIT:
                     run = False
+               if event.type  != pygame.KEYDOWN:
+                    continue
+               if event.key == pygame.K_r:
+                    lst = generate_starting_list( n, min_val, max_val )
+                    draw_info.set_list(lst)
+                    sorting = False
+               elif event.key == pygame.K_SPACE and sorting == False:
+                    sorting = True
+
+               elif event.key == pygame.K_a and not sorting:
+                    ascending = True
+               elif event.key == pygame.K_d and not sorting:
+                    ascending = False
+
+
      pygame.quit()
 
 if __name__ == "__main__":
