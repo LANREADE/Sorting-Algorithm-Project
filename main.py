51744c9b1e98bc494aa9bb6abe33e79c1e1500e7
lanre <ibrahimlanre1890@gmail.com>
2023-01-15 13:58:09 -0800

    implementing the geometrical visualization of the sorting algorithm

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 import random
 pygame.init()
 
+# the class instance taking care of the draw info event
+# the instance class that takes care of the class info
 class Drawinfo:
      BLACK = 0 , 0, 0
      WHITE = 255 , 255, 255
@@ -29,17 +31,17 @@ class Drawinfo:
           self.window = pygame.display.set_mode((width, height))
           pygame.display.set_caption("sorting Algorithm Visualization")
           self.set_list(lst)
-
+# the code auto generating the list
      def set_list(self, lst):
           self.lst = lst
           self.min_val = min( lst )
           self.max_val = max(lst)
 
           self.block_width = round((self.width - self.SIDE_PAD))/ len(lst)
-          self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+          self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val)) # to avoid the same height for the whole length r
           self.start_x = self.SIDE_PAD // 2
 
-
+# the code taking care of the draw_info and taking  draw_info as an arguement
 def draw(draw_info):
      draw_info.window.fill(draw_info.BACKGROUND_COLOR)
 
@@ -53,19 +55,27 @@ def draw(draw_info):
 
 
 
+## puting the list on display
 
-def draw_list(draw_info):
+def draw_list(draw_info, color_positions = {} , clear_bg = False):
      lst = draw_info.lst
+
+
+     if clear_bg:
+          clear_rect = (draw_info.SIDE_PAD//2 , draw_info.TOP_PAD, )
 
      for i , val in enumerate(lst):
           x = draw_info.start_x + i  * draw_info.block_width
           y = draw_info.height -  (val - draw_info.min_val) * draw_info.block_height
 
           color = draw_info.GRADIENTS[i % 3 ]
+
+          if color_positions:
+               color = color_positions[i]
           pygame.draw.rect(draw_info.window , color, (x,y ,draw_info.block_width, draw_info.height))
 
 
-
+## making the list be random while generatuing the list
 def generate_starting_list(n , min_val , max_val):
      lst = []
 
@@ -74,6 +84,26 @@ def generate_starting_list(n , min_val , max_val):
           lst.append(val)
      return lst
 
+def bubbleSort (draw_info, ascending = True ):
+     lst = draw_info.lst
+
+     for i in range(len(lst) -1):
+          for j in range(len(lst) -1 -i):
+               num1 = lst[j]
+               num2 = lst[j +1]
+
+               if (num1 > num2 and ascending) or (num1 < num2 and not asceending):
+                    lst[j], lst[j +1] = lst[j + 1] , lst[j]
+                    # draw_list
+                    yield True
+
+     return lst
+
+
+
+
+
+## The main control of the pygame  event
 def main():
      run = True
      clock = pygame.time.Clock()
@@ -121,6 +151,7 @@ if __name__ == "__main__":
 
 
 
+# the sorting algorithm
 
 
 
